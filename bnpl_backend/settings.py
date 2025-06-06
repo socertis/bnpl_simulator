c4bebfb2bc66362a3b2930e4da6ab82ab89f7dcb
Bassem <basem.alyezzat@gmail.com>
2025-06-06 23:22:31 +0300

    fixes jwt auth

--- bnpl_backend/settings.py
+++ bnpl_backend/settings.py
@@ -91,7 +91,7 @@ AUTH_USER_MODEL = 'authentication.User'
 # Django REST Framework Configuration
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
